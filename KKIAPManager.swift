17d02991cb9c6b0b784716b82be360897a629ddc
krishna <33350985+kushwaha03@users.noreply.github.com>
2021-01-06 20:14:42 +0530

    Update KKIAPManager.swift

--- KKIAPManager.swift
+++ KKIAPManager.swift
@@ -16,6 +16,21 @@ private override init() {
 // MARK: - SKProductsRequestDelegate
 
 extension KKIAPManager: SKProductsRequestDelegate {
+        // MARK: - REQUEST IAP PRODUCTS
+    func productsRequest (_ request:SKProductsRequest, didReceive response:SKProductsResponse) {
+
+        if (response.products.count > 0) {
+            iapProducts = response.products
+            for product in iapProducts{
+                let numberFormatter = NumberFormatter()
+                numberFormatter.formatterBehavior = .behavior10_4
+                numberFormatter.numberStyle = .currency
+                numberFormatter.locale = product.priceLocale
+                let price1Str = numberFormatter.string(from: product.price)
+                print(product.localizedDescription + "\nfor just \(price1Str!)")
+            }
+        }
+    }
 
 }
 
